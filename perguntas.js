cf7a9e9f995fd23b5c4596cc89938cf92e4c41ab
tiago rauber <t.rauber@escola.pr.gov.br>
2025-05-28 22:19:19 -0300

    aula 28/05 ( 4 )

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Oque é Python?',
+    'é uma linguagem de Programação'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao (
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao (
+    'Peogramação'
+    'O que é JavaScript?'
+    'JavaScript é uma linguage de Programação'
+)
