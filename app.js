c1512413631b52a71d72c0039a1f367ca97b46f0
tiago rauber <t.rauber@escola.pr.gov.br>
2025-06-05 19:16:16 -0300

    05/06 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-
+    
     cartao.innerHTML = `
         <div class="cartao__conteudo">
             <h3> ${categoria} </h3>
